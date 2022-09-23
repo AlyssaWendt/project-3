0f51339d27d4ae40e913b9394efe8b693dbd6757
Alyssa Wendt <alyssa95wendt@gmail.com>
2022-09-23 10:59:47 -0400

    last commit?

--- src/pages/TarotList.js
+++ src/pages/TarotList.js
@@ -11,8 +11,8 @@ export default function TarotList() {
   const options = {
     method: 'GET',
     headers: {
-      'X-RapidAPI-Key': process.env.apiKey,
-      'X-RapidAPI-Host': "tarot-api.p.rapidapi.com" 
+      'X-RapidAPI-Key': '341200c68bmshf3028e7a16cc7e3p145209jsn6e7b33fd36f5',
+      'X-RapidAPI-Host': 'tarot-api.p.rapidapi.com'
     }
   };
 
--- src/pages/TarotPage.js
+++ src/pages/TarotPage.js
@@ -9,8 +9,8 @@ export default function TarotPage() {
     const options = {
       method: 'GET',
       headers: {
-        'X-RapidAPI-Key': process.env.apiKey,
-        'X-RapidAPI-Host': "tarot-api.p.rapidapi.com "
+        'X-RapidAPI-Key': '341200c68bmshf3028e7a16cc7e3p145209jsn6e7b33fd36f5',
+        'X-RapidAPI-Host': 'tarot-api.p.rapidapi.com'
       }
     };
     const [cards, setCards] = useState(null);
