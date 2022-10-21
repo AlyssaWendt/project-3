cf8c79c9545c01dd023478b9e542262ff2446ee4
Alyssa Wendt <alyssa95wendt@gmail.com>
2022-10-21 16:06:38 -0400

    deploy

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,7 +5,7 @@ export default function Home() {
     <div className='Home'>
     <h1>Quick Tarot</h1>
     <img src='https://i.imgur.com/SczgLWr.png' alt='quick-img'/>
-    <h3>Quick Tarot is your place for quick tarot meaning and how tos. Everything you need to get started on you Tarot journey is right here!</h3>
+    <h3>Quick Tarot is your place for quick tarot meaning and how to. Everything you need to get started on you Tarot journey is right here!</h3>
     <a href='/tarot'> Click Here to look up Tarot card Meaning</a>
     <a href='/learn-more'> Click Here to To Learn More about Tarot</a>
     </div>
